01870369a4c5e9c678fb61b72a666929ef7e0b39
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-02-28 08:32:00 +0200

    added image protocol

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,5 +1,20 @@
 import { Product } from "@/lib/types";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function ProductCard({ product }: { product: Product }) {
-  return <div>{product.title}</div>;
+  return (
+    <div>
+      <Image
+        src={product.thumbnail}
+        alt={product.title}
+        width={200}
+        height={200}
+      />
+      <h2>{product.title}</h2>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <Link href={`/products/${product.id}`}>View Product</Link>
+    </div>
+  );
 }
