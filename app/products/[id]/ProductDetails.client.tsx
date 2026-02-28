2f46aa9678cf1d45443d8f42097b7eeb8123cd6a
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-02-28 09:06:51 +0200

    feat: added some content to ProductDetails

--- app/products/[id]/ProductDetails.client.tsx
+++ app/products/[id]/ProductDetails.client.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import { useParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { getProductById } from "@/lib/api";
+import Image from "next/image";
+import CalcPrice from "@/components/CalcPrice/CalcPrice";
+import Link from "next/link";
+import css from "./ProductDetails.module.css";
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>();
@@ -14,5 +20,15 @@ export default function ProductDetails() {
   if (isLoading) return <p>Loading...</p>;
   if (error || !data) return <p>Some error..</p>;
 
-  return <div>Product Details {id}</div>;
+  return (
+    <div>
+      Product Details {id}
+      <Image src={data.thumbnail} alt={data.title} width={200} height={200} />
+      <h2>{data.title}</h2>
+      <p>{data.description}</p>
+      <CalcPrice price={data.price} discount={data.discountPercentage} />
+      <button>Apply Promo Code</button>
+      <Link href="/products">Back to Products</Link>
+    </div>
+  );
 }
