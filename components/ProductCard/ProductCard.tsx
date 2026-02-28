66286a6d46a91acda3a6b8f627bf514cc7c3a5d9
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-02-28 11:58:30 +0200

    feat: added useQuery hook to productList & finish styles in ProductCard & ProductList components

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -2,20 +2,34 @@ import { Product } from "@/lib/types";
 import Image from "next/image";
 import Link from "next/link";
 import CalcPrice from "../CalcPrice/CalcPrice";
+import css from "./ProductCard.module.css";
+interface Props {
+  product: Product;
+}
 
-export default function ProductCard({ product }: { product: Product }) {
+export default function ProductCard({ product }: Props) {
   return (
-    <div>
-      <Image
-        src={product.thumbnail}
-        alt={product.title}
-        width={200}
-        height={200}
-      />
-      <h2>{product.title}</h2>
-      <p>{product.description}</p>
-      <CalcPrice price={product.price} discount={product.discountPercentage} />
-      <Link href={`/products/${product.id}`}>View Product</Link>
-    </div>
+    <article className={css.card}>
+      <div className={css.imageContainer}>
+        <Image
+          src={product.thumbnail}
+          alt={product.title}
+          fill
+          className={css.image}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </div>
+      <h2 className={css.title}>{product.title}</h2>
+      <p className={css.description}>{product.description}</p>
+      <div className={css.priceWrapper}>
+        <CalcPrice
+          price={product.price}
+          discount={product.discountPercentage}
+        />
+      </div>
+      <Link href={`/products/${product.id}`} className={css.button}>
+        View Product
+      </Link>
+    </article>
   );
 }
--- components/ProductsList/ProductsList.tsx
+++ components/ProductsList/ProductsList.tsx
@@ -1,16 +1,26 @@
+"use client";
 import { getProducts } from "@/lib/api";
 import { Product } from "@/lib/types";
 import ProductCard from "../ProductCard/ProductCard";
+import css from "./ProductsList.module.css";
+import { useQuery } from "@tanstack/react-query";
 
-export default async function ProductsList() {
-  const { products } = await getProducts();
+export default function ProductsList() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
+  if (isLoading) return <p>Loading...</p>;
+  if (error || !data) return <p>Some error..</p>;
   return (
-    <ul>
-      {products.map((product: Product) => (
-        <li key={product.id}>
-          <ProductCard product={product} />
-        </li>
-      ))}
-    </ul>
+    <section>
+      <ul className={css.list}>
+        {data.products.map((product: Product) => (
+          <li key={product.id}>
+            <ProductCard product={product} />
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
