5defcdcbc68eceaac12eba23a8492a2459e6a7c1
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-02-28 08:16:27 +0200

    feat: added providers component with tanstack provider

---
+++ components/Providers/Providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: Props) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
